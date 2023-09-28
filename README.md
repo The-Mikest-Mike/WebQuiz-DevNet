89d8cbd619a645d73be7c31439f1324a1acc6f4c
MIKE <eng.miguelaz@gmail.com>
2023-09-27 17:52:25 -0700

    <refactor>(renew):based on the planing phase, refactored all files

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This is a flask quiz app that allows users to answer multiple-choice questions.
 The app provides questions, answer options, and explanations for correct answers. 
 Users can navigate through the questions and get their score at the end.
 
+Structure of the folders:
+    app.py         # main Flask application file
+    templates/     # folder for HTML templates
+    static/        # folder for static files (CSS, JavaScript, images)
+    data/          # folder for data files
+        questions.json   # data file containing questions
+
+
 ## Usage
 1. Clone repository (templates and app.py) and make sure you have Flask installed:
 pip install flask
--- app.py
+++ app.py
@@ -4,11 +4,13 @@
 # - 'request': Used for handling HTTP requests and accessing request data.
 # - 'redirect': Used for redirecting users to different URLs.
 # - 'url_for': Used for generating URLs for routes defined in your Flask application.
+
 from flask import Flask, render_template, request, redirect, url_for
 
 # import 'random' module for generating random questions
 import random
-
+# import Python standard library module 'json' for reading 'questions.json' file that contains the quiz questions
+import json
 # classes and methods
 class Question:    # define 'Question' class to represent quiz questions
     def __init__(self, question, options, answer, explanation):
@@ -16,41 +18,73 @@ class Question:    # define 'Question' class to represent quiz questions
         self.options = options
         self.answer = answer
         self.explanation = explanation
-        self.user_answer = None
+        self.user_answer = None    # this value will depend on the user's interaction
 
     def check_answer(self, user_answer):
         return user_answer == self.answer
 
-# objects / constants
-app = Flask(__name__)    # initialize Flask application
+ # initialize Flask application. '__name__' determines the root path of the application and locates resources like templates and static files
+app = Flask(__name__)   
+    
+# reads quiz questions from the 'questions.json' file located in 'data' folder, parses the JSON data, and stores it in the 'questions' variable as a list of dictionaries containing quiz questions. 
+with open('data/questions.json', 'r') as json_file:
+    questions = json.load(json_file)
 
-# functions
+# shuffle the list of questions before starting a quiz
+random.shuffle(questions)
+
+# define and initialize global variable:
+current_question_index = 0
+
+# Flask '@app.route' decorator associate a function with routes handlers for the URL paths
+@app.route('/', methods=['GET', 'POST'])
 def index():
     global current_question_index
+    if request.method == 'POST':
+        current_question_index = 0
+        return redirect(url_for('question', question_index=current_question_index))
+    return render_template("start.html")
 
-    # shuffle the list of questions randomly before starting the quiz
-    random.shuffle(questions)
-    
-    if request.method == "POST":
-        user_answer = request.form.get("user_answer")
-        questions[current_question_index].user_answer = user_answer
-        current_question_index += 1
-        if current_question_index < len(questions):
-            # redirect to the "question" route if there are more questions to answer
-            return redirect(url_for("question"))
+
+# sets up a Flask route to handle requests to URLs like /question/1, /question/2, etc., where the 'question_index' parameter captures the specific question index from the URL
+
+@app.route('/question//<int:question_index>', methods=['GET', 'POST'])
+def question(question_index):
+    global current_question_index  # access the global variable
+    if request.method == 'POST':
+        # get the user's answer from the form
+        user_answer = request.form.get('user_answer')
+        # get the current question based on the current_question_index
+        current_question = questions[current_question_index]
+        
+        if current_question.check_answer(user_answer):
+            # if the answer is correct, display 'correct.html' with explanation
+            explanation = current_question.explanation
+            return render_template('correct.html', explanation=explanation)
         else:
-            correct_answers = sum(1 for q in questions if q.check_answer(q.user_answer))
-            total_questions = len(questions)
-            return render_template("result.html", correct_answers=correct_answers, total_questions=total_questions)
+            # if the answer is incorrect, display 'incorrect.html' with explanation
+            explanation = current_question.explanation
+            return render_template('incorrect.html', explanation=explanation)
+
+    if current_question_index < len(questions):
+        # render the 'question.html' template with the current question
+        return render_template('question.html', question=questions[current_question_index], question_index=question_index, current_question_index=current_question_index)
     else:
-        # display the first question if available, or redirect to the "result" page if there are no questions
-        if current_question_index < len(questions):
-            question = questions[current_question_index]
-            return render_template("question.html", question=question, current_question_index=current_question_index)
-        else:
-            return render_template("result.html", correct_answers=0, total_questions=0)  # no questions to answer, show result
+        # if all questions have been answered, redirect to the result page
+        return redirect(url_for('result'))
 
-# start the Flask application if this script is executed
+@app.route('/correct.html')
+def correct():
+    return render_template('correct.html')
+
+@app.route('/incorrect.html')
+def incorrect():
+    return render_template('incorrect.html')
+
+@app.route('/result.html')
+def result():
+    return render_template('result.html')
+
+# Start the Flask application if this script is executed
 if __name__ == "__main__":
     app.run(debug=True)
-
